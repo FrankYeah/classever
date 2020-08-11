baf73b968a7865c1552c2a867a05ce103ca33695
FrankYeah <aaa24295234@gmail.com>
2020-08-12 01:44:06 +0800

    fix img

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
 function scrollEvent(id) {
     document.getElementById(id).scrollIntoView({ behavior: 'smooth' })
 }
+
+var app = document.getElementById('app')
+var google = document.getElementById('google')
+var float = document.getElementById('float')
+
+app.addEventListener('mouseover', function() {
+    float.style.display = 'block';
+})
+
+google.addEventListener('mouseover', function() {
+    float.style.display = 'block';
+})
+
+app.addEventListener('mouseleave', function() {
+    float.style.display = 'none';
+})
+
+google.addEventListener('mouseleave', function() {
+    float.style.display = 'none';
+})
