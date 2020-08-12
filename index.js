57d788aaa52d09382167b03a3425c8519f7104cc
FrankYeah <aaa24295234@gmail.com>
2020-08-12 13:01:51 +0800

    send

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
+// 滾動到指定位置
+
 function scrollEvent(id) {
     document.getElementById(id).scrollIntoView({ behavior: 'smooth' })
 }
+
+// app store 滑鼠效果
 
 var app = document.getElementById('app')
 var google = document.getElementById('google')
@@ -20,4 +24,36 @@ app.addEventListener('mouseleave', function() {
 
 google.addEventListener('mouseleave', function() {
     float.style.display = 'none';
+})
+
+  // 訂閱
+
+var mail = document.getElementById('mail')
+var send = document.getElementById('send')
+
+send.addEventListener('click', function() {
+
+    // 正則檢查 mail 格式
+
+    if(/^([\w\.\-]){1,64}\@([\w\.\-]){1,64}$/.test(mail.value)) {
+        var data = {
+            'entry.1927707847': mail.value
+        };
+        
+        // 送出表單
+
+        $.ajax({
+            type: 'POST',
+            url: 'https://docs.google.com/forms/d/e/1FAIpQLSfyZrtwfOFBm4O3XvIyvUKx_fvq6nNBaN8J8hbaUpDlW8G0Ww/formResponse',
+            data: data,
+            contentType: 'application/json',
+            dataType: 'jsonp',
+            complete: function() {
+                alert('Subscribed')
+            }
+        });
+
+    } else {
+        alert('Please confirm email format.')
+    }
 })
